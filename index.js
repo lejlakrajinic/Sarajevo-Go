d0c5440f40b6d7c455a353f62026ed23f7953a34
lejla.krajinic <lejla.krajinic@gmail.com>
2018-08-11 11:53:01 +0200

    get users and controller

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-const express = require('express')
- 
-const app = express();
+// const express = require('express');
+// const bodyparser = require('body-parser');
+// const app = express();
+// app.use(bodyparser.urlencoded({extended: true}));
 
- 
-/*const server = app.listen(3000, function(){
-    console.log('server is running at %s .', server.address().port);
-});*/
+// const MongoClient = require('mongodb').MongoClient;
+// var MongoId = require('mongodb').ObjectID;
+// var database;
 
-/*
-var port = process.env.PORT || 3000;
+// app.use(express.static('static'))
 
-app.get("/", function(req,res){
-    res.send("Welcome to Node");
+// app.get('/user', function(req, res){
+//     database.collection('users').find().toArray((err, users) => {
+//         if (err) return console.log(err);
+//         res.setHeader('Content-Type', 'application/json');
+//         res.send(users);
+//     })
+// });
+// /*
+// app.put('/user/:user_id', function(req, res){    
+//     database.collection('users').findAndModify(
+//        {_id: new MongoId(req.params.user_id)}, // query
+//        [['_id','asc']],  // sort order
+//        {$set : {name: req.body.name, email: req.body.email, password: req.body.password}}, // replacement, replaces only the field "hi"
+//        function(err, doc) {
+//            if (err){
+//                console.warn(err.message);  // returns error if no matching object found
+//            }else{
+//                res.json(doc);
+//            }
+//        });
+// });
+// */
+// app.delete('/user/:user_id', function(req, res){
+//     database.collection('users').remove({_id: new MongoId(req.params.user_id)},
+//     function(err, data){
+//         res.json(data);
+//     });
+// });
+
+// MongoClient.connect('mongodb://localhost:27017/gosarajevo', function(err, client) {
+//     if (err) throw err;
+//     database = client.db('gosarajevo');
+//     app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+// });
+
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('localhost:27017/gosarajevo',['users']);
+ var body_parser = require('body-parser');
+ app.use(body_parser.json());
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/users', function(req,res){
+  db.users.find(function(err,docs){
+    res.json(docs)
+})
 });
 
-app.listen(port);*/
-
-const bodyparser = require("body-parser");
-const MongoClient = require('mongodb').MongoClient;
-var MongoId = require('mongodb').ObjectID;
-var database;
-
-app.use(express.static('static'))
-
-app.post('/user', function(req, res){
-    req.body._id = null;
-    console.log(req.body);
+app.post('/users', function(req, res){
     var user = req.body;
     database.collection('users').insert(user, function(err, data){
         if(err) return console.log(err);
@@ -34,38 +68,4 @@ app.post('/user', function(req, res){
     })
 });
 
-app.get('/user', function(req, res){
-    database.collection('users').find().toArray((err, users) => {
-        if (err) return console.log(err);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(users);
-    })
-});
-
-app.put('/user/:user_id', function(req, res){    
-    database.collection('users').findAndModify(
-       {_id: new MongoId(req.params.user_id)}, // query
-       [['_id','asc']],  // sort order
-       {$set : {name: req.body.name, email: req.body.email, password: req.body.password}}, // replacement, replaces only the field "hi"
-       function(err, doc) {
-           if (err){
-               console.warn(err.message);  // returns error if no matching object found
-           }else{
-               res.json(doc);
-           }
-       });
-});
-
-app.delete('/user/:user_id', function(req, res){
-    database.collection('users').remove({_id: new MongoId(req.params.user_id)},
-    function(err, data){
-        res.json(data);
-    });
-});
-
-MongoClient.connect('mongodb://localhost:27017/gosarajevo', function(err, client) {
-    if (err) throw err;
-
-    database = client.db('gosarajevo');
-    app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
-});
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
--- static/controllers/site-controller.js
+++ static/controllers/site-controller.js
@@ -1,18 +1,17 @@
 function SiteController($scope, $http){
-    console.log("EVO GA");
 
-    get_users();
+    get_users()
+
+    console.log("Hello from angular contorller!");
 
     $scope.add_user = function(){
         $http.post('/user', $scope.user).then(function(data) {
-            $scope.user = null;
             $scope.users_list.push(data);
-            //toastr.success('Car added successfully!');
         });
     }
 
     function get_users(){
-        $http.get('/user').then(function(res){
+        $http.get('/users').then(function(res){
             $scope.users_list = res.data;
         })
     }
@@ -29,9 +28,7 @@ function SiteController($scope, $http){
     $scope.update_user = function(){
         $http.put('/user/'+$scope.user._id, $scope.user).then(function(data){
           get_users();
-          //console.log($scope.car);
-          //toastr.info('You have successfully updated car!');
-          $scope.car = null;
+          $scope.user = null;
         });
       }
 
